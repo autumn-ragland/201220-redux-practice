f2da53b018ceb6d47c976e8575e216c5046c5cb1
autumn-ragland <raglanda998@gmail.com>
2020-12-20 14:45:18 -0600

    practice with one reducer

---
+++ src/reducers/counter.js
@@ -0,0 +1,12 @@
+// REDUCER => Modify store based on action
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + action.payload;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+export default counterReducer;
